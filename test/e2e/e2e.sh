1ddc998774af2a728eafb57071a6c5c7181a1ca4
Jon Huhn <nojnhuh@users.noreply.github.com>
2025-01-14 12:42:50 -0600

    Verify environment variables in e2e test

--- test/e2e/e2e.sh
+++ test/e2e/e2e.sh
@@ -27,6 +27,17 @@ kubectl create -f demo/gpu-test3.yaml
 kubectl create -f demo/gpu-test4.yaml
 kubectl create -f demo/gpu-test5.yaml
 
+function verify-env {
+  local namespace="$1"
+  local pod="$2"
+  for ctr in $(kubectl get pod -n "$namespace" "$pod" -o jsonpath='{.spec.containers[*].name}'); do
+    if ! kubectl logs -n "$namespace" "$pod" -c "$ctr" | grep -q "GPU_DEVICE_"; then
+      echo "Pod $namespace/$pod, container $ctr missing GPU_DEVICE_ environment variables"
+      exit 1
+    fi
+  done
+}
+
 kubectl wait --for=condition=Ready -n gpu-test1 pod/pod0 --timeout=120s
 kubectl wait --for=condition=Ready -n gpu-test1 pod/pod1 --timeout=120s
 gpu_test_1=$(kubectl get pods -n gpu-test1 | grep -c 'Running')
@@ -34,6 +45,8 @@ if [ $gpu_test_1 != 2 ]; then
     echo "gpu_test_1 $gpu_test_1 failed to match against 2 expected pods"
     exit 1
 fi
+verify-env gpu-test1 pod0
+verify-env gpu-test1 pod1
 
 
 kubectl wait --for=condition=Ready -n gpu-test2 pod/pod0 --timeout=120s
@@ -42,6 +55,7 @@ if [ $gpu_test_2 != 1 ]; then
     echo "gpu_test_2 $gpu_test_2 failed to match against 1 expected pod"
     exit 1
 fi
+verify-env gpu-test2 pod0
 
 kubectl wait --for=condition=Ready -n gpu-test3 pod/pod0 --timeout=120s
 gpu_test_3=$(kubectl get pods -n gpu-test3 | grep -c 'Running')
@@ -49,14 +63,17 @@ if [ $gpu_test_3 != 1 ]; then
     echo "gpu_test_3 $gpu_test_3 failed to match against 1 expected pod"
     exit 1
 fi
+verify-env gpu-test3 pod0
 
 kubectl wait --for=condition=Ready -n gpu-test4 pod/pod0 --timeout=120s
 kubectl wait --for=condition=Ready -n gpu-test4 pod/pod1 --timeout=120s
 gpu_test_4=$(kubectl get pods -n gpu-test4 | grep -c 'Running')
 if [ $gpu_test_4 != 2 ]; then
-    echo "gpu_test_4 $gpu_test_4 failed to match against 1 expected pods"
+    echo "gpu_test_4 $gpu_test_4 failed to match against 2 expected pods"
     exit 1
 fi
+verify-env gpu-test4 pod0
+verify-env gpu-test4 pod1
 
 kubectl wait --for=condition=Ready -n gpu-test5 pod/pod0 --timeout=120s
 gpu_test_5=$(kubectl get pods -n gpu-test5 | grep -c 'Running')
@@ -64,6 +81,7 @@ if [ $gpu_test_5 != 1 ]; then
     echo "gpu_test_5 $gpu_test_5 failed to match against 1 expected pod"
     exit 1
 fi
+verify-env gpu-test5 pod0
 
 # test that deletion is fast (less than the default grace period of 30s)
 # see https://github.com/kubernetes/kubernetes/issues/127188 for details
